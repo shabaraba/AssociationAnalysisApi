4f9d4b97a6688f35b4637222c2f7967f556a5d9a
shabaraba <work.shabaraba@gmail.com>
2023-01-25 16:52:46 +0900

    ISSUE #2 不要ファイルとコメントを削除

--- app/app/app_stack.py
+++ app/app/app_stack.py
@@ -20,29 +20,8 @@ class AssociationAnalysisApiStack(Stack):
             code= _lambda.DockerImageCode.from_image_asset('lambda'),
             timeout= Duration.seconds(60 * 5)
         )
-        # lambda_layer = _lambda.LayerVersion(
-        #     self, "LambdaLayerLayer",
-        #     compatible_runtimes=[_lambda.Runtime.PYTHON_3_7],
-        #     code=_lambda.AssetCode("lambda_layer"),
-        # )
-        # my_lambda = _lambda.Function(
-        #     self, 'AssociationAnalysisLambda',
-        #     description='アソシエーション分析をするためのlambda',
-        #     runtime=_lambda.Runtime.PYTHON_3_7,
-        #     code=_lambda.Code.from_asset('lambda'),
-        #     layers=[lambda_layer],
-        #     handler='association_analyze.handler',
-        # )
         # create API Gateway
         api = _apigateway.LambdaRestApi(
             self, 'AssociationAnalysisApiGateway', handler=my_lambda, proxy=True)
         analysis_api = api.root.add_resource('analyze')
         analysis_api.add_method('POST')
-        # item = items.add_resource("{item}")
-
-        # item.add_method("GET")  # GET /items/{item}
-
-        # # the default integration for methods is "handler", but one can
-        # # customize this behavior per method or even a sub path.
-        # item.add_method(
-        #     "DELETE", apigateway.HttpIntegration("http://amazon.com"))
